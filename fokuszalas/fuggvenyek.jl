60776f7d4a84e8b7218ac21230d89075ca36f5c9
Illes Gergo <illes.gergo1@gmail.com>
2023-04-16 18:03:14 +0200

    Fokusz2D

--- fokuszalas/fuggvenyek.jl
+++ fokuszalas/fuggvenyek.jl
@@ -5,16 +5,25 @@ function gaussNyalab(x, z, t, A, lambda0)
     z0 = pi * w0^2 ./ lambda0
     zM = f ./ (1 + (f ./ z0) .^ 2)
     z_ = z .- zM
-    recR =@. (-1 / f + z_ .* (1 / f^2 + 1 ./ z0 .^ 2)) ./ ((1 - z_ ./ f) .^ 2 + (z_ ./ z0) .^ 2)
-    recWz =@. sqrt((1 ./ z0) ./ ((1 - z_ ./ f) .^ 2 + (z_ ./ z0) .^ 2) * pi ./ lambda0)
+    recR = @. (-1 / f + z_ .* (1 / f^2 + 1 ./ z0 .^ 2)) ./ ((1 - z_ ./ f) .^ 2 + (z_ ./ z0) .^ 2)
+    recWz = @. sqrt((1 ./ z0) ./ ((1 - z_ ./ f) .^ 2 + (z_ ./ z0) .^ 2) * pi ./ lambda0)
 
-    gouy =@. atan(z_ ./ z0)
+    gouy = @. atan(z_ ./ z0)
 
-    V =@. A * w0 .* recWz .* exp(-x .^ 2 .* recWz .^ 2) .* exp(-1im .* (k .* (z_) - omega0 * t + k .* x .^ 2 ./ 2 .* recR - gouy))
+    V = @. A * w0 .* recWz .* exp(-x .^ 2 .* recWz .^ 2) .* exp(-1im .* (k .* (z_) - omega0 * t + k .* x .^ 2 ./ 2 .* recR - gouy))
     return V
 end
 
-function superPos(startI, endI, nu, spektrum, zGrid, xGrid)
+function superPos(startI, endI, nu, spektrum, zGrid, xGrid::LazyGrids.GridSL)
+    supPos = zeros(Complex{Float64}, size(zGrid)..., nthreads())
+    @threads for ind in startI:endI
+        supPos[:, :, threadid()] += gaussNyalab(xGrid, zGrid, t, spektrum[ind], 3e8 / nu[ind])
+    end
+    return dropdims(sum(supPos, dims=3),dims=3)
+end
+
+
+function superPos(startI, endI, nu, spektrum, zGrid, xGrid::Number)
     supPos = zeros(Complex{Float64}, size(zGrid))
     for ind in startI:endI
         supPos += gaussNyalab(xGrid, zGrid, t, spektrum[ind], 3e8 / nu[ind])
--- fokuszalas/main.jl
+++ fokuszalas/main.jl
@@ -1,18 +1,26 @@
-using Plots, DelimitedFiles, FFTW
+using Plots, DelimitedFiles, FFTW, LazyGrids, Base.Threads
 
 include("fuggvenyek.jl")
 
 
-f = 5e-2 / 2;
+f = 5e-2;
 w0 = 1e-2;
 t = 0
-z = range(-1e-2,1e-2,5000) .+ f
+z = range(-1e-2, 1e-2, 5000) .+ f
+z_ = range(-1e-2, 1e-2, 5000) .+ f
+x = range(-1e-2, 1e-2, 1000)
+
+Z, X = ndgrid(z_, x)
 
 plotlyjs()
 rawData = readdlm(pwd() * "/fokuszalas/ATHz.txt", ',')
 nu = rawData[:, 1] * 1e12
 spektrum = rawData[:, 2]
-#plot(nu, spektrum)
+#display(plot(spektrum))
 
-@time eredmeny = superPos(2,10000,nu,spektrum,z,0)
-plot(real(eredmeny))
+@time eredmeny = superPos(2, 1000, nu, spektrum, z, 0)
+display(plot(real(eredmeny)))
+
+@time eredmeny2D = superPos(2, 1000, nu, spektrum, Z, X)
+#eredmeny2D = dropdims(eredmeny2D, dims=3)
+display(heatmap(real(eredmeny2D)))
