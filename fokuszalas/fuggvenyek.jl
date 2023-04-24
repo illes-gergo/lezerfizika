b336ddebc6dca83c4e24fd6555aa1540a8bd72ca
Illés Gergő <illes.gergo1@gmail.com>
2023-04-24 09:39:18 +0200

    AutoCommit

--- fokuszalas/fuggvenyek.jl
+++ fokuszalas/fuggvenyek.jl
@@ -1,32 +1,31 @@
-function gaussNyalab(x, z, t, A, lambda0)
+function gaussNyalab(x, z, t, A, lambda0, fazis)
 
-    omega0 = lambda0 / 3e8 / 2 / pi
+    omega0 = 3e8 / lambda0 * 2 * pi
     k = 2 * pi ./ lambda0
     z0 = pi * w0^2 ./ lambda0
-    zM = f ./ (1 + (f ./ z0) .^ 2)
-    z_ = z .- zM
+    # zM = f ./ (1 + (f ./ z0) .^ 2)
+    z_ = z
     recR = @. (-1 / f + z_ .* (1 / f^2 + 1 ./ z0 .^ 2)) ./ ((1 - z_ ./ f) .^ 2 + (z_ ./ z0) .^ 2)
     recWz = @. sqrt((1 ./ z0) ./ ((1 - z_ ./ f) .^ 2 + (z_ ./ z0) .^ 2) * pi ./ lambda0)
-
     gouy = @. atan(z_ ./ z0)
-
-    V = @. A * w0 .* recWz .* exp(-x .^ 2 .* recWz .^ 2) .* exp(-1im .* (k .* (z_) - omega0 * t + k .* x .^ 2 ./ 2 .* recR - gouy))
+    # println(omega0)
+    V = @. A * w0 .* recWz .* exp(-x .^ 2 .* recWz .^ 2) .* exp(-1im .* (k .* (z_) - omega0 * t + k .* x .^ 2 ./ 2 .* recR - gouy + fazis))
     return V
 end
 
-function superPos(startI, endI, nu, spektrum, zGrid, xGrid::LazyGrids.GridSL)
+function superPos(startI, endI, nu, spektrum, zGrid, xGrid::LazyGrids.GridSL, t, fazis)
     supPos = zeros(Complex{Float64}, size(zGrid)..., nthreads())
     @threads for ind in startI:endI
-        supPos[:, :, threadid()] += gaussNyalab(xGrid, zGrid, t, spektrum[ind], 3e8 / nu[ind])
+        supPos[:, :, threadid()] += gaussNyalab(xGrid, zGrid, t, spektrum[ind], 3e8 / nu[ind], fazis[ind])
     end
-    return dropdims(sum(supPos, dims=3),dims=3)
+    return dropdims(sum(supPos, dims=3), dims=3)
 end
 
 
-function superPos(startI, endI, nu, spektrum, zGrid, xGrid::Number)
+function superPos(startI, endI, nu, spektrum, zGrid, xGrid::Number, t, fazis)
     supPos = zeros(Complex{Float64}, size(zGrid))
     for ind in startI:endI
-        supPos += gaussNyalab(xGrid, zGrid, t, spektrum[ind], 3e8 / nu[ind])
+        supPos += gaussNyalab(xGrid, zGrid, t, spektrum[ind], 3e8 / nu[ind], fazis[ind])
     end
     return supPos
 end
--- fokuszalas/main.jl
+++ fokuszalas/main.jl
@@ -1,26 +1,31 @@
-using Plots, DelimitedFiles, FFTW, LazyGrids, Base.Threads
+using Plots, DelimitedFiles, FFTW, LazyGrids, Base.Threads, DSP
 
 include("fuggvenyek.jl")
-
+default(size=[800, 600])
 
 f = 5e-2;
 w0 = 1e-2;
 t = 0
-z = range(-1e-2, 1e-2, 5000) .+ f
-z_ = range(-1e-2, 1e-2, 5000) .+ f
-x = range(-1e-2, 1e-2, 1000) .* 2 
+mult = 1.0
+z = range(-1e-2, 1e-2, 5000) .* 2 .+ mult * f
+z_ = range(-1e-2, 1e-2, 5000) .* 2 .+ mult * f
+x = range(-1e-2, 1e-2, 1000) .* 2
 
 Z, X = ndgrid(z_, x)
 
 plotlyjs()
-rawData = readdlm(pwd() * "/fokuszalas/ATHz.txt", ',')
-nu = rawData[:, 1] * 1e12
+rawData = readdlm(pwd() * "/fokuszalas/FocusData.txt", ',')
+nu = rawData[:, 1]
 spektrum = rawData[:, 2]
-display(plot(spektrum))
+fazis = unwrap(rawData[:, 3])
 
-@time eredmeny = superPos(2, 1000, nu, spektrum, z, 0)
+#display(plot(fazis))
+
+ @time eredmeny = superPos(2, 1000, nu, spektrum, z, 0, t, fazis)
 display(plot(real(eredmeny)))
 
-@time eredmeny2D = superPos(2, 1000, nu, spektrum, Z, X)
+
+@time eredmeny2D = superPos(2, 1000, nu, spektrum, Z, X, t, fazis)
 #eredmeny2D = dropdims(eredmeny2D, dims=3)
 display(heatmap(real(eredmeny2D)))
+
