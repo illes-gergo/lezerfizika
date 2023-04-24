bb7b484fc39396ef383a63d167fa543217558816
Illés Gergő <illes.gergo1@gmail.com>
2023-04-24 14:34:26 +0200

    Fókusz kókusz megszerelve

--- fokuszalas/main.jl
+++ fokuszalas/main.jl
@@ -5,11 +5,11 @@ default(size=[800, 600])
 
 f = 5e-2;
 w0 = 1e-2;
-t = 0
+t = -0.46e-10
 mult = 1.0
 z = range(-1e-2, 1e-2, 5000) .* 2 .+ mult * f
-z_ = range(-1e-2, 1e-2, 5000) .* 2 .+ mult * f
-x = range(-1e-2, 1e-2, 1000) .* 2
+z_ = range(-1e-2, 1e-2, 500) .* 0.20 .+ mult * f
+x = range(-1e-2, 1e-2, 500) .* 0.20
 
 Z, X = ndgrid(z_, x)
 
@@ -22,10 +22,10 @@ fazis = unwrap(rawData[:, 3])
 #display(plot(fazis))
 
  @time eredmeny = superPos(2, 1000, nu, spektrum, z, 0, t, fazis)
-display(plot(real(eredmeny)))
+display(plot(z,real(eredmeny)))
 
 
 @time eredmeny2D = superPos(2, 1000, nu, spektrum, Z, X, t, fazis)
 #eredmeny2D = dropdims(eredmeny2D, dims=3)
-display(heatmap(real(eredmeny2D)))
+display(heatmap(x,z_,real(eredmeny2D)))
 
